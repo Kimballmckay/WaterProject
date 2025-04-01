693925315f28f76da2bc0a2ac70320c0991cc364
Mckay <kimballmckay12@gmail.com>
2025-04-01 15:59:34 -0600

    deploy

--- backend/WaterProject.API/Program.cs
+++ backend/WaterProject.API/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000")
+            policy.WithOrigins("http://localhost:3000", "https://agreeable-wave-0c4d2151e.6.azurestaticapps.net")
                 .AllowCredentials()
                 .AllowAnyHeader()
                 .AllowAnyMethod();
