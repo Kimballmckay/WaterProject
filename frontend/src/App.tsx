1a36176b1c6d7e92369b230c5babdae0adb3b63f
Mckay <kimballmckay12@gmail.com>
2025-04-01 15:14:41 -0600

    FinalVideos commits

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DonatePage from './pages/DonatePage';
 import CartPage from './pages/CartPage';
 import { CartProvider } from './context/CartContext';
+import AdminProjectsPage from './pages/AdminProjectsPage';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
               element={<DonatePage />}
             />
             <Route path="/cart" element={<CartPage />} />
+            <Route path="/adminprojects" element={<AdminProjectsPage />} />
           </Routes>
         </Router>
       </CartProvider>
