1aba4a699eb8a9a4e3f33ef16996ad9a287df4ea
Mckay <kimballmckay12@gmail.com>
2025-04-01 16:29:46 -0600

    update

--- frontend/src/api/ProjectsAPI.ts
+++ frontend/src/api/ProjectsAPI.ts
@@ -5,7 +5,7 @@ interface FetchProjectsResponse {
   totalNumProjects: number;
 }
 
-const API_URL = 'https://waterproject-kimball-backend1.azurewebsites.net/Water';
+const API_URL = 'https://waterproject-kimball-backend.azurewebsites.net/Water';
 
 export const fetchProjects = async (
   pageSize: number,
